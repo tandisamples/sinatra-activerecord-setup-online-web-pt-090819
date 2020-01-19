2de1e049652b79452c217610e3bbb41087af3d1e
Tandi Samples <tandisamples@gmail.com>
2020-01-19 16:40:29 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_31_214350) do
+ActiveRecord::Schema.define(version: 2020_01_02_222828) do
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
